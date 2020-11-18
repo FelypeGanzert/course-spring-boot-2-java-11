a59e8b98cb9b19985503b068973567ad7a19efd9
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-11-18 01:43:55 -0300

    Service layer, component registration

---
+++ src/main/java/com/felypeganzert/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.felypeganzert.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felypeganzert.course.entities.User;
+import com.felypeganzert.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
