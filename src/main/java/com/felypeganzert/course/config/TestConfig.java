74f44c233f35bab5161786e3960fb5fef4355709
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-11-27 23:20:33 -0300

    Payment, one-to-one association

--- src/main/java/com/felypeganzert/course/config/TestConfig.java
+++ src/main/java/com/felypeganzert/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.felypeganzert.course.entities.Category;
 import com.felypeganzert.course.entities.Order;
 import com.felypeganzert.course.entities.OrderItem;
+import com.felypeganzert.course.entities.Payment;
 import com.felypeganzert.course.entities.Product;
 import com.felypeganzert.course.entities.User;
 import com.felypeganzert.course.entities.enums.OrderStatus;
@@ -77,7 +78,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
